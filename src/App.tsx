2b09ea974e120cf1a5286f052f47b5959f021d09
chrl0t <charlotte_i@live.co.uk>
2021-05-10 14:06:11 +0100

    Add inputs and button to App and handleChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className='App'></div>;
-}
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    // setTodoList([...todoList, task]);
+  };
+
+  return (
+    <div className='App'>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input
+            type='text'
+            placeholder='Task...'
+            name='tasl'
+            onChange={handleChange}
+          ></input>
+          <input
+            type='number'
+            placeholder='Deadline in Days'
+            name='deadline'
+          ></input>
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='todoList'></div>
+    </div>
+  );
+};
 
 export default App;
