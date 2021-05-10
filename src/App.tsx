8a7ad4875b3b3435f6a2dd0efbf05d35f91902fe
chrl0t <charlotte_i@live.co.uk>
2021-05-10 14:14:41 +0100

    Store tasks in todoList state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
+import { Task } from "./Interfaces";
 
 const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<Task[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
@@ -15,7 +16,10 @@ const App: FC = () => {
   };
 
   const addTask = (): void => {
-    // setTodoList([...todoList, task]);
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadline(0);
   };
 
   return (
@@ -25,13 +29,16 @@ const App: FC = () => {
           <input
             type='text'
             placeholder='Task...'
-            name='tasl'
+            name='task'
+            value={task}
             onChange={handleChange}
           ></input>
           <input
             type='number'
             placeholder='Deadline in Days'
             name='deadline'
+            value={deadline}
+            onChange={handleChange}
           ></input>
         </div>
         <button onClick={addTask}>Add Task</button>
